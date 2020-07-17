04399c66b9a6fa0c2aec8588bfc8e188293b36ef
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-07-16 20:14:40 -0400

    No changes

--- middleware/logger.js
+++
@@ -1,8 +0,0 @@
-// custom logger middleware
-module.exports = () => {
-    return (req, res, next) => {
-            const time = new Date().toISOString()
-            console.log(`[${time}] ${req.method} ${req.url}`)
-            next()
-        }               
-} 
