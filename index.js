4d420a9f570b6970853654d05f8ddcd97186d0c6
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-07-16 20:20:02 -0400

    Update .env file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const server = require('./server.js');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`\n* Server Running on http://localhost:${port} *\n`);
